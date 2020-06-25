7c227d763e1e83caacdc67a36ef9b32d50c21de5
Noemmi <noemimontuori0@gmail.com>
2020-06-25 23:37:11 +0200

    html Login

--- intoTheJob/src/app/app-routing.module.ts
+++ intoTheJob/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [{
       path: 'login',
       component: LoginComponent
-      }];
+      },
+      { path: '', redirectTo: '/login', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
