bf988c675317a117354758b1466357b4c9df2b27
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2020-06-25 23:00:08 +0200

    Fixed Error Bootstrap

--- intoTheJob/src/app/app.module.ts
+++ intoTheJob/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    
-  ],
-  providers: [NgbModule],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
