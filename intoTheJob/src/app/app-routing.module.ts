83227d107e548400dc1ffb869285cc6bcb03075a
Claudia <claudia.damico94@gmail.com>
2020-06-30 23:01:22 +0200

    Register HTML component

--- intoTheJob/src/app/app-routing.module.ts
+++ intoTheJob/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [{
       path: 'login',
       component: LoginComponent
       },
-      { path: '', redirectTo: '/login', pathMatch: 'full' }];
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      {path: 'register',
+      component: RegisterComponent
+    }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- intoTheJob/src/app/app.module.ts
+++ intoTheJob/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
